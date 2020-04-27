ac74849dfa84607db02722c5e3a72bd9830b2ca7
Álex J. Cantos <a.j.cantos@gmail.com>
2020-04-27 18:02:08 +0200

    Now we get the nickname of the user and store it server-side

--- application.py
+++ application.py
@@ -1,13 +1,30 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, session, render_template, request
+from flask_session import Session
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-socketio = SocketIO(app)
 
+# COnfigure secret key
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+socketio = SocketIO(app)
+Session(app)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    nickname = session.get('nickname')
+    if nickname is None:
+        nickname = 'Guest User'
+    return render_template('index.html', nickname=nickname)
+
+@app.route("/store-nickname", methods=["POST"])
+def store_nickname():
+    nickname = request.form.get('nickname')
+    session['nickname'] = nickname
+    return nickname
--- static/index.js
+++ static/index.js
@@ -1,8 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
-
     // Launch modal to get user
     $('#modalGetUser').modal('show');
 
-    // Other actions
+    // Assign onclick handler for user modal
+    document.getElementById('acceptModalGetUser').addEventListener('click', function() { get_user(); });
+    //document.getElementById('acceptModalGetUser').onclick = get_user; // Alternative
 
+    // Assign onkeyup handler for user modal
+    document.getElementById('nicknameModalGetUser').addEventListener('keyup', function(event) { get_user_from_keyboard(event); });
 });
+
+/**
+ * Get the nickname from the user with ENTER key
+ */
+function get_user_from_keyboard(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        
+        // Trigger the button element with a click
+        document.getElementById("acceptModalGetUser").click();
+    }
+}
+
+/**
+ * Get the nickname from the user
+ */
+function get_user() {
+    // Get nickname from the modal
+    const nickname = document.querySelector('#nicknameModalGetUser').value
+
+    // Hide the modal
+    $('#modalGetUser').modal('hide');
+
+    // Insert the new nickname in the DOM
+    document.querySelector('#header-lateral-nickname').innerHTML = nickname;
+
+    /* Send the nickname back to the server */
+    // Create request and open it
+    const request = new XMLHttpRequest();
+    request.open('POST', '/store-nickname');
+
+    // Add data to send with request
+    const data = new FormData();// Send the data//
+    data.append('nickname', nickname);
+
+    // Send the data
+    request.send(data);
+}
